cebe9001d74742c1a89c05be5cda9ba906ecff85
Faraz Karimi <faraz.karimi1990@gmail.com>
2020-12-08 20:02:50 +0330

    get location

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,14 +4,21 @@ import Geolocation from 'geolocation';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
+  const [loc, setLoc] = React.useState({});
 
-  // React.useEffect(() => {
-  //   Geolocation.multiply(3, 7).then(setResult);
-  // }, []);
+  React.useEffect(() => {
+    Geolocation.multiply(3, 7).then(setResult);
+    Geolocation.getLocation(2, 3).then((res) => {
+      const response = JSON.parse(res);
+      setLoc(response);
+    })
+  }, []);
 console.log(result)
   return (
     <View style={styles.container}>
-      <Text>Result:</Text>
+      <Text>lat: {loc.lat}</Text>
+      <Text>lng: {loc.lng}</Text>
+
     </View>
   );
 }
--- ios/Geolocation.swift
+++ ios/Geolocation.swift
@@ -1,8 +1,47 @@
+
+import CoreLocation
+import UserNotifications
+
 @objc(Geolocation)
 class Geolocation: NSObject {
+    var locationManager = CLLocationManager()
+    override init() {
+        super.init()
+        locationManager.requestWhenInUseAuthorization()
+    }
 
     @objc(multiply:withB:withResolver:withRejecter:)
     func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
         resolve(a*b)
     }
+
+     @objc(getLocation:withB:withResolver:withRejecter:)
+     func getLocation(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+         locationManager.requestWhenInUseAuthorization()
+         var currentLoc: CLLocation!
+         if(CLLocationManager.authorizationStatus() == .authorizedWhenInUse ||
+         CLLocationManager.authorizationStatus() == .authorizedAlways) {
+            currentLoc = locationManager.location
+            print(currentLoc.coordinate.latitude)
+            print(currentLoc.coordinate.longitude)
+            let json = "{" + "lat"
+            
+            let jsonObject: [String: Any] = [
+                "lat": currentLoc.coordinate.longitude,
+                "lng": currentLoc.coordinate.latitude,
+            ]
+            
+            do {
+                let jsonData = try JSONSerialization.data(withJSONObject: jsonObject, options: JSONSerialization.WritingOptions()) as NSData
+                let jsonString = NSString(data: jsonData as Data, encoding: String.Encoding.utf8.rawValue) as! String
+          
+                resolve(jsonString)
+            } catch _ {
+                print ("JSON Failure")
+            }
+            
+         }
+     }
+    
+    
 }
