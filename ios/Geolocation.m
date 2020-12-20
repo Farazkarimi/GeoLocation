e9e53618b7fe0f5b2df60acd3a16b8bff022943f
Faraz Karimi <faraz.karimi1990@gmail.com>
2020-12-20 15:26:42 +0330

    get location in background

--- ios/Geolocation.m
+++ ios/Geolocation.m
@@ -9,4 +9,8 @@ RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
  RCT_EXTERN_METHOD(getLocation:(float)a withB:(float)b
                    withResolver:(RCTPromiseResolveBlock)resolve
                    withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(getServerResponse:(NSString)token withOffline:(BOOL) offline withTimeInterval: (Double)timeInterval
+                  withResolver:(RCTPromiseResolveBlock)resolve
+                  withRejecter:(RCTPromiseRejectBlock)reject)
 @end
